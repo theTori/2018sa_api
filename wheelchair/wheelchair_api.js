b7884e4374685e0958c9fce03971e1cf4d478857
theTori <41200579+theTori@users.noreply.github.com>
2018-07-15 20:35:53 +0900

    Add files via upload

---
+++ wheelchair/wheelchair_api.js
@@ -0,0 +1,41 @@
+
+var isRunning = 1;
+var notRunning = 1;
+
+URLreceive: {
+    //http server require
+}
+events: {
+    //행동을 URL Scheme에서 가져오는 내용 넣어야.
+}
+setup: {
+    //이름 바꾸는 함수
+    //긴 랜덤토큰 생성 함수(RSA 참고)
+}
+astp: {
+    var distanceWall;
+    //if(distanceWall <= 안전한 거리)
+}
+token: {
+    var passWord;
+    //인증키가 맞는지 확인하는 과정(RSA 참고)
+}
+speed: {
+    var wheelchairSpeed;
+    //입력 들어오면 if문으로 상황 구분 후 ++나 --로 속도 조절
+}
+go: {
+
+}
+back: {
+
+}
+locQ: {
+    //https://github.com/google/google-api-nodejs-client 참고해서 진행할 예정
+}
+turnL: {
+
+}
+turnR: {
+    //모터 조정하는 npm 패키지 받아 조정.
+}
